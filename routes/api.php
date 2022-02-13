63cc69e759cce2232d53c957f1a881269db08118
Mun <ieymis@hotmail.com>
2022-02-13 16:03:10 +0300

    <user>

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('users/all', 'UserController@index');
+Route::get('users/create', 'UserController@create');
+Route::get('users/store', 'UserController@store');
+Route::get('users/show', 'UserController@show');
+Route::get('users/edit/{id}', 'UserController@edit');
+Route::get('users/update/{id}' , 'UserController@update');
+Route::get('users/destroy/{id}', 'UserController@destroy');
+
+
